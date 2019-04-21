3c04f800b7f4acc48d99a6c29e27be3e0880d4de
Jameson Campbell <jcampbell@flexterapp.com>
2019-04-21 17:41:01 -0500

    design & temp api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,26 +9,28 @@ function initMap() {
         zoom: 15
     });
     infoWindow = new google.maps.InfoWindow;
-    
+
     // Draw route
     var drawingManager = new google.maps.drawing.DrawingManager({
         drawingMode: google.maps.drawing.OverlayType.MARKER,
         drawingControl: true,
         drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
         },
-        markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+        markerOptions: {
+            icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+        },
         circleOptions: {
-          fillColor: '#ffff00',
-          fillOpacity: 1,
-          strokeWeight: 5,
-          clickable: false,
-          editable: true,
-          zIndex: 1
+            fillColor: '#ffff00',
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1
         }
-      });
-      drawingManager.setMap(map);
+    });
+    drawingManager.setMap(map);
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -41,21 +43,18 @@ function initMap() {
             var latitude = pos.lat;
             var longitude = pos.lng;
 
-            console.log(latitude);
-            console.log(longitude);
-            // console.log(pos.lat);
-            // console.log(pos.lng);
-
-            var queryURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&&APPID=9c7fdaf4c8e115f87c5bd3abe0cd5762";
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&&APPID=9c7fdaf4c8e115f87c5bd3abe0cd5762";
 
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
                 console.log(response)
-                $(`#temp`).html((Math.round((response.main.temp) - 273.15) * 9/5 + 32 ));
-                $(`#temp`).append(response.weather[0].description);
+                //$(`#weather`).html((Math.round((response.main.temp) - 273.15) * 9 / 5 + 32));
+                // $(`#weather`).append(response.weather[0].description);
+                //$(`#icon`).html(response.weather[0].icon);
 
+                $(`#weather`).html(imageConverter(response));
 
             })
 
@@ -78,4 +77,19 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
+}
+
+function imageConverter(response) {
+
+    return `
+        <div class="d-inline-block font-weight-light">
+            <p>${((Math.round((response.main.temp) - 273.15) * 9 / 5 + 32))}°F</p>
+        </div>
+        <div class="d-inline-block text-capitalize font-weight-light">
+            <p>${response.weather[0].description}</p>
+         </div>
+        <div class="d-inline-block">
+            <img src="http://openweathermap.org/img/w/${response.weather[0].icon}.png"/>
+        </div>
+    `
 }
